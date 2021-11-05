eed597b48237f308677d91a3e4c69bfe9c7a6f68
enrique <nohunenrique@gmail.com>
2021-11-04 22:51:29 -0500

    Implementación de registro y login

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::post('login', [RegisterController::class, 'login']);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+// Se accede a las apis mediante un token.
 Route::middleware('auth:api')->group(function () {
     Route::get('/perfil', [ UserController::class, 'perfil']);
 });
